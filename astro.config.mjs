4b71cad3709875bce705605c4fcac74d7765f48b
Lilith River <lilith@imazen.io>
2023-03-26 10:30:20 -0600

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,31 @@
 import { defineConfig } from 'astro/config';
+import { KNOWN_LANGUAGES } from './src/consts.ts';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://astro.build`,
+  integrations: [
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), 
+  sitemap({
+	i18n: {
+        defaultLocale: 'en',   // All urls that don't contain `es` or `fr` after `https://www.srcset.tips/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: 'en',         // The `defaultLocale` value must present in `locales` keys
+          es: 'es',
+          de: 'de',
+		  sv: 'sv',
+		  zh: 'zh-CN',
+		  it: 'it',
+		  fr: 'fr',
+		  ja: 'ja',
+        },
+      },
+  })],
+  site: `https://www.srcset.tips`
 });
