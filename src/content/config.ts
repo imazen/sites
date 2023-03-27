49f406700a418cd3c9c4e67ffef43a43cc00b0a2
Lilith River <lilith@imazen.io>
2023-03-27 01:11:45 -0600

    Don't require the lang: frontmatter to always be "en-US"

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const docs = defineCollection({
 		title: z.string().default(SITE.title),
 		description: z.string().default(SITE.description),
 		sidebar_sort_priority: z.number().optional(),
-		lang: z.literal('en-us').default(SITE.defaultLanguage),
+		lang: z.string().default(SITE.defaultLanguage),
 		dir: z.union([z.literal('ltr'), z.literal('rtl')]).default('ltr'),
 		image: z
 			.object({
